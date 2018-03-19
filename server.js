7cd31cc5fa50f8c25550eed9909deb742ea0fa5e
Leandro Späth <leandro.spaeth@gmail.com>
2018-03-19 11:58:11 +0100

    correct API endpoints

--- server.js
+++ server.js
@@ -32,6 +32,20 @@ const sendData = (url, payload, deviceId, XPin) => {
     request(options)
 }
 
+//converts payload for madavi API to Luftdaten API, because it does not require value_type prefixes
+function convertToLuftdatenPayload(madaviPayload) {
+    let sensorPayloadLuftdaten = [];
+    madaviPayload.forEach((elem) => {
+        let luftElem = JSON.parse(JSON.stringify(elem)); //duplicate object without reference
+        let vt = luftElem.value_type;
+        if (vt.includes('_')) {
+            luftElem.value_type = vt.substr(vt.indexOf('_') + 1);
+        }
+        sensorPayloadLuftdaten.push(luftElem);
+    });
+    return sensorPayloadLuftdaten;
+}
+
 ttn.data(appID, accessKey)
     .then((client) => {
         client.on('uplink', (devID, payload) => {
@@ -41,62 +55,69 @@ ttn.data(appID, accessKey)
 
             if (payload.payload_fields.pm10 && payload.payload_fields.pm25) {
                 const pmPayload = [{
-                    value_type: 'P1',
-                    value: payload.payload_fields.pm10.toString()
-                },
-                {
-                    value_type: 'P2',
-                    value: payload.payload_fields.pm25.toString()
-                }
+                        value_type: 'SDS_P1',
+                        value: payload.payload_fields.pm10.toString()
+                    },
+                    {
+                        value_type: 'SDS_P2',
+                        value: payload.payload_fields.pm25.toString()
+                    }
                 ]
-                sendData('https://api.luftdaten.info/v1/push-sensor-data/', pmPayload, deviceId, 1)
-                const hpm_pmPayload = [{
-                    value_type: 'P1',
-                    value: payload.payload_fields.pm10.toString()
-                },
-                {
-                    value_type: 'P2',
-                    value: payload.payload_fields.pm25.toString()
-                }
-                ]
-                sendData('https://api-rrd.madavi.de/data.php', hpm_pmPayload, deviceId, 1)
+                sendData('https://api.luftdaten.info/v1/push-sensor-data/', convertToLuftdatenPayload(pmPayload), deviceId, 1)
+                sendData('https://api-rrd.madavi.de/data.php', pmPayload, deviceId, 1)
             }
 
-            const sensorPayload = []
-            var valueTypes = 0
+            // need to differentiate the sensors based on the sensor values provided (because the API requires it)
+            let xPin = 0;
+            let p = payload.payload_fields;
+            let apiString = {};
+            if (p.temperature && !p.humidity && !p.pressure) {
+                xPin = 13; //DS18B20
+                apiString.temperature = "DS18B20_temperature";
+            } else if (p.temperature && p.humidity && !p.pressure) {
+                xPin = 7; //DHT22
+                apiString.temperature = "temperature";
+                apiString.humidity = "humidity";
+            } else if (p.temperature && !p.humidity && p.pressure) {
+                xPin = 3; //BMP180
+                apiString.temperature = "BMP_temperature";
+                apiString.pressure = "BMP_pressure";
+            } else if (p.temperature && p.humidity && p.pressure) {
+                xPin = 11; //BME280
+                apiString.temperature = "BME280_temperature";
+                apiString.humidity = "BME280_humidity";
+                apiString.pressure = "BME280_pressure";
+            }
+
+            const sensorPayloadMadavi = []
             if (payload.payload_fields.temperature) {
-                sensorPayload.push({
-                    value_type: 'temperature',
+                sensorPayloadMadavi.push({
+                    value_type: apiString.temperature || 'temperature',
                     value: payload.payload_fields.temperature.toString()
                 });
-                valueTypes += 1;
             }
             if (payload.payload_fields.humidity) {
-                sensorPayload.push({
-                    value_type: 'humidity',
+                sensorPayloadMadavi.push({
+                    value_type: apiString.humidity || 'humidity',
                     value: payload.payload_fields.humidity.toString()
                 });
-                valueTypes += 2;
             }
             if (payload.payload_fields.pressure) {
-                sensorPayload.push({
-                    value_type: 'pressure',
+                sensorPayloadMadavi.push({
+                    value_type: apiString.pressure || 'BMP_pressure',
                     value: payload.payload_fields.pressure.toString()
                 });
-                valueTypes += 4;
             }
-
-            const xPinMapping = {
-                0b001: 13, //DS18B20
-                0b011: 7, //DHT22
-                0b101: 3, //BMP180
-                0b111: 11, //BME280
-            }
-            var xPin = xPinMapping[valueTypes]
 
             if (xPin > 0 && xPin !== undefined) {
-                sendData('https://api.luftdaten.info/v1/push-sensor-data/', sensorPayload, deviceId, xPin)
-                sendData('https://api-rrd.madavi.de/data.php', sensorPayload, deviceId, xPin)
+                /*log('apiData: ' + JSON.stringify({
+                    deviceId: deviceId,
+                    xPin: xPin,
+                    sensorPayloadMadavi: sensorPayloadMadavi,
+                    sensorPayloadLuftdaten: convertToLuftdatenPayload(sensorPayloadMadavi)
+                }));*/
+                sendData('https://api.luftdaten.info/v1/push-sensor-data/', convertToLuftdatenPayload(sensorPayloadMadavi), deviceId, xPin)
+                sendData('https://api-rrd.madavi.de/data.php', sensorPayloadMadavi, deviceId, xPin)
             }
         })
     })
@@ -104,4 +125,3 @@ ttn.data(appID, accessKey)
         log(err)
         process.exit(1)
     })
-  
